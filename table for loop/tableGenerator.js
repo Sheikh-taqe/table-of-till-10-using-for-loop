0404b352b8354f373ad2b12a8464d3bfeb15ca20
Sheikh-taqe <162182944+Sheikh-taqe@users.noreply.github.com>
2024-03-06 21:56:14 -0800

    Add files via upload

---
+++ table for loop/tableGenerator.js
@@ -0,0 +1,8 @@
+// JavaScript code to generate the table
+for (let i = 1; i <= 10; i++) {
+    document.write("<tr>");
+    document.write("<td>" + "2 x " + i + "</td>");
+    document.write("<td>" + (2 * i) + "</td>");
+    document.write("</tr>");
+  }
+  
